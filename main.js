41c1129b8857401f062503ae383722882e0fc1d5
LaToddra Belton <latoddra.belton@codeimmersives.com>
2020-07-07 12:40:09 -0400

    finished

--- main.js
+++ main.js
@@ -6,9 +6,9 @@ const onlyOdds = function (arr) {
   let newArr = [];
   for (const nums of arr) {
     if (nums % 2 === 1) {
-      newArr.push(nums)
+      newArr.push(nums);
     }
-  } return newArr
+  } return newArr;
 
 }
 
@@ -18,12 +18,37 @@ const onlyEvens = function (arr) {
   let newArr = [];
   for (const nums of arr) {
     if (nums % 2 === 0) {
-      newArr.push(nums)
+      newArr.push(nums);
     }
-  } return newArr
+  } return newArr;
 }
 
 
+/*-----*/
+const shortNamesOnly = function(arr){
+  let shortNames = [];
+  for(const names of arr){
+    if(names.length < 7){
+      shortNames.push(names);
+
+    }
+    
+  }
+return shortNames;
+}
+  
+
+/*-----*/
+
+const dNames = function(arr){
+  let dsOnly = [];
+  for(const names of arr){
+  if(names.startsWith('D')){
+dsOnly.push(names)
+  }
+ 
+  } return dsOnly
+}
 /*********************************
  * OUR CODE BELOW; DO NOT TOUCH! *
  *********************************/
